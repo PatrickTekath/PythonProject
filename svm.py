3edc7a403c13b592113ceda0ef0437b9764daf91
tekatpmz <tekatpmz@CIP-PYTHON-20.vdi.ruhr-uni-bochum.de>
2015-09-28 12:51:44 +0200

    working TfIdf vector but currently not able to predict data correctlya to vectors to be able to fit the ForestClassifier

--- svm.py
+++ svm.py
@@ -1,48 +1,51 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 import numpy as np
 import pandas as pd
 
 # Create variables that include the training and the test data
-trainRecipes = pd.read_json("train.json")
-testRecipes = pd.read_json("test.json")
+trainRecipes = pd.read_json("train_mod.json")
+testRecipes = pd.read_json("test_mod.json")
 
+# extract ingredients for each recipe of the json file for both training and test data
 trainRecipes['ingredients_string'] = [' '.join(i).strip() for i in trainRecipes['ingredients']]
 testRecipes['ingredients_string'] = [' '.join(i).strip() for i in testRecipes['ingredients']]
 
-ingredientsTraining = trainRecipes['ingredients']
-ingredientsTest = testRecipes['ingredients']
+# convert ingredients of recipes into a panda list containing the ingredients specific a recipe
+ingredientsTraining = trainRecipes['ingredients_string']
+ingredientsTest = testRecipes['ingredients_string']
 
-vectorizertr = TfidfVectorizer(stop_words='english')
-vectorizedIngredients = vectorizertr.fit_transform(trainRecipes)
+# initialize a Tfidf Vectorizer to determine the tf and idf
+# term frequency and inverse document frequency
+vectorTraining = TfidfVectorizer()
+vectorTest = TfidfVectorizer()
 
-predictors_Training = vectorizedIngredients
+# train the vectorizer on the training ingredients to determine most common ingredients
+vectorizedIngredients = vectorTraining.fit_transform(ingredientsTraining)
 
+#train the vectorizer on the test ingredients
+# vectorizedIngredientsTest = vectorTest.fit(ingredientsTest)
+vectorizedIngredientsTest = vectorTest.transform(ingredientsTest)
+
+# contains the cuisine for each recipe
 cuisine = trainRecipes['cuisine']
 
-cuisineArray = np.array(pd.Series.get_values(cuisine))
-
+# Initialize a random forest classifier
 forestCLS = RandomForestClassifier(n_estimators=15)
 
-'''
-xtraining = np.random.standard_normal((100,2))
-xtest = np.random.standard_normal((100000,2))
+# train the Classifier with the data for the ingredients and the cuisines
+forestCLS.fit(vectorizedIngredients,cuisine)
 
-ytraining = np.empty(100)
-ytest = np.empty(100000)
+print vectorizedIngredients.shape
+print
+print vectorizedIngredientsTest.shape
 
-for i in range(0,100,1):
-    ytraining[i] = np.sign(xtraining[i][0])
+# predict the data
+predictions = forestCLS.predict(vectorizedIngredients)
 
-for i in range(0,100000,1):
-    ytest[i] = np.sign(xtest[i][0])
+# store the predictions into a DataFrame
+trainRecipes['predictions'] = predictions
 
-print type(xtraining)
-print type(ytraining) 
-
-forestCLS = RandomForestClassifier(n_estimators= 15)
-
-forestCLS.fit(xtraining,ytraining)
-forestCLS.predict(xtest)
-#print(forestCLS.score(xtest,ytest))
-'''
+# print predictions
+print (trainRecipes['predictions'])
