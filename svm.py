f7fdcff2f1528273cfccd3f14836a68fcc83934e
tekatpmz <tekatpmz@CIP-PYTHON-20.vdi.ruhr-uni-bochum.de>
2015-09-28 15:37:33 +0200

    prediction works^^

--- svm.py
+++ svm.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 
 # Create variables that include the training and the test data
-trainRecipes = pd.read_json("train_mod.json")
-testRecipes = pd.read_json("test_mod.json")
+trainRecipes = pd.read_json("train.json")
+testRecipes = pd.read_json("test.json")
 
 # extract ingredients for each recipe of the json file for both training and test data
 trainRecipes['ingredients_string'] = [' '.join(i).strip() for i in trainRecipes['ingredients']]
@@ -18,15 +18,16 @@ ingredientsTest = testRecipes['ingredients_string']
 
 # initialize a Tfidf Vectorizer to determine the tf and idf
 # term frequency and inverse document frequency
-vectorTraining = TfidfVectorizer()
-vectorTest = TfidfVectorizer()
+vectorTraining = TfidfVectorizer(stop_words='english')
 
 # train the vectorizer on the training ingredients to determine most common ingredients
 vectorizedIngredients = vectorTraining.fit_transform(ingredientsTraining)
-
 #train the vectorizer on the test ingredients
-# vectorizedIngredientsTest = vectorTest.fit(ingredientsTest)
-vectorizedIngredientsTest = vectorTest.transform(ingredientsTest)
+vectorizedIngredientsTest = vectorTraining.transform(ingredientsTest)
+
+#Make a copy of the different vectorized ingredients
+predictors_Test = vectorizedIngredientsTest
+predictors_Training = vectorizedIngredients
 
 # contains the cuisine for each recipe
 cuisine = trainRecipes['cuisine']
@@ -35,17 +36,13 @@ cuisine = trainRecipes['cuisine']
 forestCLS = RandomForestClassifier(n_estimators=15)
 
 # train the Classifier with the data for the ingredients and the cuisines
-forestCLS.fit(vectorizedIngredients,cuisine)
-
-print vectorizedIngredients.shape
-print
-print vectorizedIngredientsTest.shape
+forestCLS.fit(predictors_Training,cuisine)
 
 # predict the data
-predictions = forestCLS.predict(vectorizedIngredients)
+predictions = forestCLS.predict(predictors_Test)
 
 # store the predictions into a DataFrame
-trainRecipes['predictions'] = predictions
+testRecipes['predictions'] = predictions
 
 # print predictions
-print (trainRecipes['predictions'])
+print (testRecipes['predictions'])
