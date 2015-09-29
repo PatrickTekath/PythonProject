7ee2ffa94522ec812f0090c94dda69ebf3a0a2bd
tekatpmz <tekatpmz@CIP-PYTHON-03.vdi.ruhr-uni-bochum.de>
2015-09-29 16:08:39 +0200

    Working classifier for a random forest. Initialized a file to try out deep learning

--- randomForest.py
+++ randomForest.py
@@ -1,28 +1,52 @@
-from itertools import chain
-
-import nltk
-from pandas import Series, DataFrame
-from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import classification_report
-import sklearn.metrics
 
-import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
 
-# is most likely to get programmed
-# make training files
+# Create variables that include the training and the test data
 trainRecipes = pd.read_json("train.json")
-trainRecipes['ingredients_string'] = [' '.join(z).strip() for z in trainRecipes['ingredients']]
-
-# make test files
 testRecipes = pd.read_json("test.json")
-testRecipes['ingredients_string'] = [' '.join(z).strip() for z in testRecipes['ingredients']]
 
-# create a copy of trainRecipes and only filter out the ingredients
-copyOfIngredients_Training = trainRecipes['ingredients_string']
-copyOfIngredients_Test = testRecipes['ingredients_string']
+# extract ingredients for each recipe of the json file for both training and test data
+trainRecipes['ingredients_string'] = [' '.join(i).strip() for i in trainRecipes['ingredients']]
+testRecipes['ingredients_string'] = [' '.join(i).strip() for i in testRecipes['ingredients']]
 
-print copyOfIngredients_Training[1]
+# convert ingredients of recipes into a panda list containing the ingredients specific a recipe
+ingredientsTraining = trainRecipes['ingredients_string']
+ingredientsTest = testRecipes['ingredients_string']
+
+# initialize a Tfidf Vectorizer to determine the tf and idf
+# term frequency and inverse document frequency
+vectorTraining = TfidfVectorizer(stop_words='english')
+
+# train the vectorizer on the training ingredients to determine most common ingredients
+vectorizedIngredients = vectorTraining.fit_transform(ingredientsTraining)
+#train the vectorizer on the test ingredients
+vectorizedIngredientsTest = vectorTraining.transform(ingredientsTest)
+
+#Make a copy of the different vectorized ingredients
+predictors_Test = vectorizedIngredientsTest
+predictors_Training = vectorizedIngredients
+
+# contains the cuisine for each recipe
+cuisine = trainRecipes['cuisine']
+
+# Initialize a random forest classifier
+forestCLS = RandomForestClassifier(n_estimators=1000, max_features='auto', criterion='gini', n_jobs=-1)
+
+# train the Classifier with the data for the ingredients and the cuisines
+forestCLS.fit(predictors_Training,cuisine)
+
+# predict the data
+predictions = forestCLS.predict(predictors_Test)
+
+# store the predictions into a DataFrame
+testRecipes['cuisine'] = predictions
+
+subID = testRecipes['id']
+subCuisine = testRecipes['cuisine']
+
+#print ("{0},{1}".format(testRecipes['id'],testRecipes['predictions']))
+
+testRecipes[['id','cuisine']].to_csv('results.csv', index = False)
