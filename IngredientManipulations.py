8cc69c8c9e362844e8f11f2fbdd5d871239bd3c1
Gerk <gerkpaba@CIP-PYTHON-29.vdi.ruhr-uni-bochum.de>
2015-10-01 15:30:33 +0200

    Fully cleaned and documented the Ingredient Manipulation and CSV saving modules.

--- IngredientManipulations.py
+++ IngredientManipulations.py
@@ -1,117 +1,131 @@
+"""Normalize and clean Ingredients.
+
+This module is used to normalize the Ingredients. It offers a naive way (normalizeIngredientsNaive) and
+a way that uses nltk tagging (normalizeIngredientsNLTK), as well as a simple cleanup
+in which the tokens of multi-token ingredients are concatenated with an underscore (normalizeIngredientsCO).
+"""
+
 from readFile import *
 import nltk
-from nltk.metrics.distance import edit_distance
 
-def extractIngredientsFromRecipe(data, recipeNumber):#Extracts the ingredients from a recipe into a list
+def extractIngredientsFromRecipe(data, recipeNumber):
+	"""Extract the ingredients from a recipe and return a list"""
 	ingredients = (data[::][recipeNumber]['ingredients'][x] for x in range (0, len(data[::][recipeNumber]['ingredients'])))
 	return list(ingredients)
 
-def extractCuisineFromRecipe(data, recipeNumber):#Extracts the ingredients from a recipe into a list
+def extractCuisineFromRecipe(data, recipeNumber):
+	"""Extract and return the cuisine from a given recipe"""
 	cuisine = (data[recipeNumber]['cuisine'])
 	return (cuisine)
 
-def normalizeIngredientsOld(ingredients):#Normalizes the ingredients in a given list
-	#data structures to hold ingredients with one or multiple tokens respectively
+def normalizeIngredientsCO(ingredients):
+	"""Concatenate the tokens of ingredients with multiple tokens"""
+	
+	"""data structures to hold ingredients with one or multiple tokens respectively"""
 	oneToken = []
 	moreTokens = []
 
-	#iterating over every ingredient in the recipe
+	"""iterate over every ingredient in the recipe"""
 	for ingredient in ingredients:
-		#check if the ingredient only has one token; if so, add to "one token" list
+		"""check if the ingredient only has one token; if so, add to one token list; if not, add to multiple tokens list"""
 		if ingredient == ingredient.split(" ")[0]:
 			oneToken.append(ingredient)
-		#else the ingredient has more than one token and may need to be normalized
 		else:
 			moreTokens.append(ingredient)
 		
-	#Since they need no normalization, all single token ingredients are added to the normalized Ingredients list
+	"""all single token ingredients are added to the normalized Ingredients list"""
 	normalizedIngredients = oneToken
 			
-	#all multiple token ingredients are split into their respective tokens
+	"""multiple token ingredients are added back to the normalized ingredient list as a string"""
+	for ingredient in moreTokens:		
+		normalizedIngredients.append('_'.join(ingredient.split(" ")))
+
+	return normalizedIngredients
+
+def normalizeIngredientsNaive(ingredients):
+	"""Normalize the ingredients in a given list using a naive attempt"""
+	
+	"""data structures to hold ingredients with one or multiple tokens respectively"""
+	oneToken = []
+	moreTokens = []
+
+	"""iterate over every ingredient in the recipe"""
+	for ingredient in ingredients:
+		"""check if the ingredient only has one token; if so, add to one token list; if not, add to multiple tokens list"""
+		if ingredient == ingredient.split(" ")[0]:
+			oneToken.append(ingredient)
+		else:
+			moreTokens.append(ingredient)
+		
+	"""Since they need no normalization, all single token ingredients are added to the normalized Ingredients list"""
+	normalizedIngredients = oneToken
+			
+	"""all multiple token ingredients are split into their respective tokens"""
 	for ingredient in moreTokens:
 		normalizedIngredient = []
 		ingredientTokenList = ingredient.split(" ")
 	
-		#Those tokens are discarded if they look like an adjective or irrelevant addition; else they are kept 
+		"""Those tokens are discarded if they look like an adjective or irrelevant addition; else they are kept""" 
 		for token in ingredientTokenList:
 			if token.endswith("ed"): pass
 			elif token.endswith("ing"): pass
 			elif token.endswith("boneless"): pass
 			elif token.endswith("skinless"): pass
 			else : normalizedIngredient.append(token)
-	
-		#the list is converted back to a String
-		normalizedIngredientString = normalizedIngredient[0]
-		del(normalizedIngredient[0])
-		for token in normalizedIngredient:
-			normalizedIngredientString = normalizedIngredientString + " " + token
 			
-		#The normalized String is added back to the normalized Ingredient list		
-		normalizedIngredients.append(normalizedIngredientString)
+		"""The normalized Ingredient is added back to the normalized Ingredient list as a string"""		
+		normalizedIngredients.append('_'.join(normalizedIngredient))
 
 	return normalizedIngredients
 
-
-#Testing ground
-#data = readTrainingData()
-#ingredients = extractIngredientsFromRecipe(data, 549)
-#normal = normalizeIngredients(ingredients)
-#print normal
-
-#print edit_distance("oil", "cooking oil")
-
-def normalizeIngredients(ingredients):#normalizes the ingredients in a given list using nltk
-		
+def normalizeIngredientsNLTK(ingredients):
+	""""normalize the ingredients in a given list using nltk"""
+	
+	"""Data structure to store the normalized Ingredients"""
 	normalizedIngredients=[]
 	
+	"""Clean and append every ingredient in the list"""
 	for ingredient in ingredients:
 		normalizedIngredients.append(cleanIngredient(ingredient))
 		
 	return normalizedIngredients
 	
-def cleanIngredient(ingredient):#cleans a given ingredient from unneeded parts using nltk
+def cleanIngredient(ingredient):
+	"""clean a given ingredient from unneeded parts using nltk"""
 	
-	#data structure to hold the cleaned tokens
+	"""data structure to hold the cleaned tokens"""
 	cleaned_tokens = []
 	
-	#tokenize the ingredient
+	"""tokenize the ingredient"""
 	tokens = nltk.word_tokenize(ingredient)
 	
-	#tag the ingredient
+	"""tag the ingredient"""
 	tagged_tokens = nltk.pos_tag(tokens)
 	
-	#iterate the tokens and remove unneeded words
+	"""iterate the tokens and remove unneeded words"""
 	for i in range(0,len(tagged_tokens)):
 		if ((tagged_tokens[i][1])!='JJ' and (tagged_tokens[i][1])!='VBD' and (tagged_tokens[i][1])!='VBG'): cleaned_tokens.append(tagged_tokens[i][0])
 	
-	#check if any words made it through; if not, just use the original tokens
+	"""check if any words made it through; if not, just use the original tokens"""
 	if not cleaned_tokens:
 		for i in range(0,len(tagged_tokens)): cleaned_tokens.append(tagged_tokens[i][0])
 	
-	#create a new string to store the cleaned ingredient
-	cleaned_ingredient = ""
-	
-	#add all cleaned tokens to the string
-	for token in cleaned_tokens:
-		cleaned_ingredient += token
-		cleaned_ingredient += '+'
+	""""create a new string to store the cleaned ingredient"""
+	cleaned_ingredient = '_'.join(cleaned_tokens)
 		
-	#return the string without the ending whitespace
-	return cleaned_ingredient[:-1]
+	return cleaned_ingredient
 
-#data = readTrainingDataMod()
-#9: Garlic
-#recipe_number=0
-#print extractIngredientsFromRecipe(data, recipe_number)
-#print normalizeIngredients(extractIngredientsFromRecipe(data, recipe_number))
-#print normalizeIngredientsOld(extractIngredientsFromRecipe(data, 9))
-
-def normalizePandas(pandas_dataframe):#normalizes pandas ingredient list and returns a pandas ingredient dataframe
+def normalizePandasNLTK(pandas_dataframe):
+	"""normalize pandas ingredient series and returns a pandas ingredient series"""
+	
+	"""Convert Pandas dataframe to list"""
 	df2list = list(pandas_dataframe['ingredients_string'])
 
+	"""Iterate over all recipes and normalize the ingredients"""
 	for i in range(len(df2list)):
-		df2list[i] = normalizeIngredients(df2list[i])
+		df2list[i] = normalizeIngredientsNLTK(df2list[i])
 
+	
 	result = [' '.join(i).strip() for i in ingredientsTest]
 	
 	return result
