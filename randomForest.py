d84d727f3f2441c794b0a552e9d7da3a751b01a0
tekatpmz <tekatpmz@CIP-PYTHON-22.vdi.ruhr-uni-bochum.de>
2015-10-01 11:36:23 +0200

    Documentation has beguna random forest. Initialized a file to try out deep learning

--- randomForest.py
+++ randomForest.py
@@ -1,12 +1,17 @@
+import sklearn
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble.forest import ExtraTreesClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+import readFile as rf
 
 import numpy as np
 import pandas as pd
+import sklearn.cross_validation as cv
+
 
 # Create variables that include the training and the test data
-trainRecipes = pd.read_json("train.json")
-testRecipes = pd.read_json("test.json")
+trainRecipes, testRecipes = rf.readDocuments()
 
 # extract ingredients for each recipe of the json file for both training and test data
 trainRecipes['ingredients_string'] = [' '.join(i).strip() for i in trainRecipes['ingredients']]
@@ -22,24 +27,25 @@ vectorTraining = TfidfVectorizer(stop_words='english')
 
 # train the vectorizer on the training ingredients to determine most common ingredients
 vectorizedIngredients = vectorTraining.fit_transform(ingredientsTraining)
-#train the vectorizer on the test ingredients
+
+# evaluate important ingredients from the test data
 vectorizedIngredientsTest = vectorTraining.transform(ingredientsTest)
 
-#Make a copy of the different vectorized ingredients
+# Make a copy of the different vectorized ingredients
 predictors_Test = vectorizedIngredientsTest
 predictors_Training = vectorizedIngredients
 
 # contains the cuisine for each recipe
 cuisine = trainRecipes['cuisine']
 
-# Initialize a random forest classifier
-forestCLS = RandomForestClassifier(n_estimators=1000, max_features='auto', criterion='gini', n_jobs=-1)
+# initialize a SVM 
+linSVM = SVC(kernel = 'linear', C=1)
 
-# train the Classifier with the data for the ingredients and the cuisines
-forestCLS.fit(predictors_Training,cuisine)
+# fit the svm to the data
+linSVM.fit(predictors_Training, cuisine)
 
 # predict the data
-predictions = forestCLS.predict(predictors_Test)
+predictions = linSVM.predict(predictors_Test)
 
 # store the predictions into a DataFrame
 testRecipes['cuisine'] = predictions
@@ -47,6 +53,5 @@ testRecipes['cuisine'] = predictions
 subID = testRecipes['id']
 subCuisine = testRecipes['cuisine']
 
-#print ("{0},{1}".format(testRecipes['id'],testRecipes['predictions']))
-
+# write results to csv file to be able to submit it to kaggle
 testRecipes[['id','cuisine']].to_csv('results.csv', index = False)
--- readFile.py
+++ readFile.py
@@ -1,17 +1,33 @@
 #imports
-import json
+import pandas as pd
 from pprint import pprint
 
+def readDocuments(pathTraining="train.json", pathTesting="test.json"):
+    # read documents using pandas
+    trainRecipes = pd.read_json(pathTraining)
+    testRecipes  = pd.read_json(pathTesting)
+    return trainRecipes, testRecipes
 
-#open json files in data
-with open('train.json') as data_file:
-    data = json.load(data_file)
-
-#print last ingredient of first recipe
-#pprint(data[0]['ingredients'][len(data[0]['ingredients'])-1])
-
-
-#uniqueCuisines = (data[::][i]['cuisine'] for i in range(0,len(data)-1))
-#uniqueCuisines = set(uniqueCuisines)
-
-#print uniqueCuisines
+def processDocuments():
+    trainRecipes, testRecipes = readDocuments()
+    
+    # extract ingredients for each recipe of the json file for both training and test data
+    trainRecipes['ingredients_string'] = [' '.join(i).strip() for i in trainRecipes['ingredients']]
+    testRecipes ['ingredients_string'] = [' '.join(i).strip() for i in testRecipes ['ingredients']]
+    
+    # initialize a Tfidf Vectorizer to determine the tf and idf
+    # term frequency and inverse document frequency
+    vectorTraining = TfidfVectorizer(stop_words='english')
+    
+    # train the vectorizer on the training ingredients to determine most common ingredients
+    vectorizedIngredients = vectorTraining.fit_transform(ingredientsTraining)
+    
+    # evaluate important ingredients from the test data
+    vectorizedIngredientsTest = vectorTraining.transform(ingredientsTest)
+    
+    # Make a copy of the different vectorized ingredients
+    predictors_Test = vectorizedIngredientsTest
+    predictors_Training = vectorizedIngredients
+    
+    # contains the cuisine for each recipe
+    cuisine = trainRecipes['cuisine']
